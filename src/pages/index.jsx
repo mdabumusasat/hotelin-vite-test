48dfe6ac44f89efe28e961fa5600d5ce24e1bab0
Abu Musa <96811902+mdabumusasat@users.noreply.github.com>
2026-05-23 09:43:23 +0600

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Layout from '../layouts/Layout';
 import Banner from '../section/Home1/Banner';
-import Checkout from "../section/home1/Checkout";
-import About from "../section/home1/About";
-import Destination from "../section/home1/Destination";
-import Feature from "../section/home1/Feature";
-import Suite from "../section/home1/Suite";
-import Explore from "../section/home1/Explore";
-import Testimonial from "../section/home1/Testimonial";
-import News from "../section/home1/News";
-import Instragram from "../section/home1/Instragram";
+import Checkout from "../section/Home1/Checkout";
+import About from "../section/Home1/About";
+import Destination from "../section/Home1/Destination";
+import Feature from "../section/Home1/Feature";
+import Suite from "../section/Home1/Suite";
+import Explore from "../section/Home1/Explore";
+import Testimonial from "../section/Home1/Testimonial";
+import News from "../section/Home1/News";
+import Instragram from "../section/Home1/Instragram";
 
 function Home1() {
     return (
